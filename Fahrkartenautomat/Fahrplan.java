0c9d24466f6bf6ed477d93b734c3b36b05c13524
Robert Wießner <robert.wiessner1@gmx.de>
2022-12-14 11:05:33 +0100

    4.3 Implementierung des Fahrplans
    Fahrplanklasse ausfüllen nach derm Lösen des Flussdiagramms welches im
    Unterricht erarbeitet wurde

--- Fahrkartenautomat/Fahrplan.java
+++ Fahrkartenautomat/Fahrplan.java
@@ -10,9 +10,35 @@ public class Fahrplan {
         fahrzeit = fahrzeit + 8; // Fahrzeit Hbf -> Spandau
 
         if (haltInSpandau == true) {
-            fahrzeit = fahrzeit + 2; // Halt in Spandau
+            	fahrzeit = fahrzeit + 2; // Halt in Spandau
         }
-        
+       	if (richtungHamburg){
+		fahrzeit += 96;
+	}
+		
+	if (haltInStendal){
+		fahrzeit += 16;  
+	}else{
+		fahrzeit += 6;  // wird umfahren Umfahren 
+	}
+
+	switch (endetIn){
+		case 'h':
+			fahrzeit += 62;  
+			System.out.println(String.format("Fahrziel Hannover beträgt %d Minuten",fahrzeit));
+			break;
+		case 'b':
+			fahrzeit += 50;  
+			System.out.println(String.format("Fahrziel Braunschweig beträgt %d Minuten",fahrzeit));
+			break;
+		case 'w': 
+			fahrzeit += 29;  
+			System.out.println(String.format("Fahrziel Wolfsburg beträgt %d Minuten",fahrzeit));
+			break;
+		default: 
+			System.out.println("Fehler im Zielcomputer - wir fahren ins Nichts!!!");	
+	}
+
         // Hier das Programm weiterschreiben
     }
 }
