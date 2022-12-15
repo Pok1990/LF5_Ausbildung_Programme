d3a15b9557891e8d8474a694fbacadb11e87c7b3
Robert Wießner <robert.wiessner1@gmx.de>
2022-12-15 10:47:32 +0100

    A4.4 Fallunterscheidung
    mit Eingabecheck ob der Monat Valid ist.

---
+++ Fahrkartenautomat/Kalender.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+// 4.5 Fallunterscheidung
+public class Kalender {
+	public static void main(String[] args) {
+		int tage = 0;
+		int monat;
+		Scanner tastatur = new Scanner(System.in);
+
+		System.out.print("Wie lautet der Monat (1-12): ");
+		monat = tastatur.nextInt();
+		if (monat < 1 || monat > 12){
+			System.out.println("Monat ungültig - Programm beendet sich");
+			System.exit(0);
+		}
+		switch (monat){
+		case 2:
+			tage= 28;
+			break;
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			tage = 31;
+			break;
+		default:
+			tage = 30;
+		} 
+
+		System.out.printf("Der %d-te Monat im Jahr hat %d Tage.\n", monat, tage);
+		tastatur.close();
+	}
+}
